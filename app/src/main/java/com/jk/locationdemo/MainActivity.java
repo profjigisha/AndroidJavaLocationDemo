33830d861f22896f0484d12318d9fbcba594aa74
jkp <jigishapatel28@gmail.com>
2021-02-09 17:58:49 -0500

    Initial Commit

---
+++ app/src/main/java/com/jk/locationdemo/MainActivity.java
@@ -0,0 +1,44 @@
+package com.jk.locationdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnOpenMap;
+    private Button btnShowNavigation;
+    private TextView tvLocation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.btnOpenMap = findViewById(R.id.btnOpenMap);
+        this.btnOpenMap.setOnClickListener(this);
+
+        this.btnShowNavigation = findViewById(R.id.btnShowNavigation);
+        this.btnShowNavigation.setOnClickListener(this);
+
+        tvLocation = findViewById(R.id.tvLocation);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view != null){
+            switch (view.getId()){
+                case R.id.btnOpenMap:
+
+                    break;
+
+                case R.id.btnShowNavigation:
+
+                    break;
+            }
+        }
+    }
+}
