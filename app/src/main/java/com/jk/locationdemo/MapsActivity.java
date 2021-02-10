8f3ff40c31b757a83c085d0dd75227f32431e954
jkp <jigishapatel28@gmail.com>
2021-02-09 21:23:01 -0500

    Added Location Updates On Map

--- app/src/main/java/com/jk/locationdemo/MapsActivity.java
+++ app/src/main/java/com/jk/locationdemo/MapsActivity.java
@@ -1,19 +1,30 @@
 package com.jk.locationdemo;
 
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.fragment.app.FragmentActivity;
 
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private final String TAG = this.getClass().getCanonicalName();
+    private final Float DEFAULT_ZOOM = 15.0f;
+    private LocationManager locationManager;
+    private LocationCallback locationCallback;
+    private LatLng currentLocation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +34,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        this.locationManager = LocationManager.getInstance();
+        this.locationManager.checkPermissions(this);
+
+        if (this.locationManager.locationPermissionGranted) {
+
+            locationCallback = new LocationCallback() {
+                @Override
+                public void onLocationResult(LocationResult locationResult) {
+                    if (locationResult == null) {
+                        return;
+                    }
+
+                    for (Location loc : locationResult.getLocations()) {
+
+                        currentLocation = new LatLng(loc.getLatitude(), loc.getLongitude());
+//                        mMap.animateCamera(CameraUpdateFactory.newLatLng(currentLocation));
+                        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, DEFAULT_ZOOM));
+                        mMap.addMarker(new MarkerOptions().position(currentLocation).title("You're here !"));
+
+                        Log.e(TAG, "Lat : " + loc.getLatitude() + "\nLng : " + loc.getLongitude());
+                        Log.e(TAG, "Number of locations" + locationResult.getLocations().size());
+                    }
+                }
+            };
+
+            this.locationManager.requestLocationUpdates(this, locationCallback);
+        }
     }
 
     /**
@@ -42,5 +81,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        if (googleMap != null) {
+            googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+            this.setupGoogleMapScreenSetting(googleMap);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        this.locationManager.stopLocationUpdates(this, this.locationCallback);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        this.locationManager.requestLocationUpdates(this, this.locationCallback);
+    }
+
+    private void setupGoogleMapScreenSetting(GoogleMap gMap) {
+        gMap.setBuildingsEnabled(true);
+        gMap.setIndoorEnabled(true);
+        gMap.setTrafficEnabled(false);
+
+        UiSettings myUiSettings = gMap.getUiSettings();
+
+        myUiSettings.setZoomControlsEnabled(true);
+        myUiSettings.setZoomGesturesEnabled(true);
+        myUiSettings.setMyLocationButtonEnabled(true);
+        myUiSettings.setScrollGesturesEnabled(true);
+        myUiSettings.setRotateGesturesEnabled(true);
     }
 }
